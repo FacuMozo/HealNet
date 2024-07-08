e636520ce5962d76842d5e75ecd9bbf504645247
FacuMozo <facumozo98@gmail.com>
2024-07-08 20:30:28 -0300

    Se agregan iconos del Appointment Form Se agregan estilos para ordenar componentes, agregar bordes, text bold, bg-gradient del button, hover del button, Se agrega consumo de emails para select desde API actualizando el estado de emails al recibir respuesta de la API

--- src/components/AppointmentForm/AppointmentForm.jsx
+++ src/components/AppointmentForm/AppointmentForm.jsx
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import email from '../../images/iconos/email.png'
+import calendar from '../../images/iconos/calendar.png'
+import phone from '../../images/iconos/phone.png'
 import './AppointmentForm.scss';
 
 const AppointmentForm = () => {
+    const [emails, setEmails] = useState([]);
+
+    useEffect(() => {
+        fetch('https://dummyapi.online/api/users')
+            .then((response) => response.json())
+            .then((data) => {
+                const emailOptions = data.map(user => user.email);
+                setEmails(emailOptions);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+
     return (
-        <section className="appointment-form">
-            <div className="container">
-                <h2>Make an Appointment</h2>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <select id="email" className="form-control">
-                            {/* Options fetched from API */}
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="phone">Phone</label>
-                        <input type="text" id="phone" className="form-control" placeholder="Enter your phone number" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="date">Date</label>
-                        <input type="date" id="date" className="form-control" />
-                    </div>
-                    <button type="submit" className="submit-btn">Submit</button>
-                </form>
+        // <section className=" m-0">
+        <div className='container  p-0'>   
+            <div className='appointment-form'>
+                <div className="container">
+                    <h2 className='text-bold'>Easily book an appointment in 3 simple steps.</h2>
+                    <form className='row'>
+                        <div className="col form-group">
+                            <label htmlFor="email"><img className='me-2' src={email} alt="" />Email</label>
+                            <select id="email" className="form-control" required>
+                                <option className='placeholder-option' value="" disabled selected>Enter Your Email Address</option>
+                                {emails.map((email, index) => (
+                                    <option key={index} value={email}>
+                                        {email}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="col form-group">
+                            <label htmlFor="phone"><img className='me-2' src={ phone} alt="" />Phone</label>
+                            <input type="text" id="phone" className="form-control" placeholder="Enter your phone number" />
+                        </div>
+                        <div className="col form-group">
+                            <label htmlFor="date"><img className='me-2' src={calendar} alt="" />Date</label>
+                            <input type="date" id="date" className="form-control" />
+                        </div>
+                        <div className='col'>
+                            <button type=" submit" className="submit-btn">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </section>
+        </div>
+        // </section>
     );
 }
 
