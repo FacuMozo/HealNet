8549399c42d614d120aa244b7c12e93bf1749249
FacuMozo <facumozo98@gmail.com>
2024-07-16 16:57:52 -0300

    se corrige comilla no valida

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ import test4 from '../../images/testimony/testimony-4.png'
 import esfera from '../../images/testimony/esfera.png'
 
 const Testimonials = () => {
-    const text1 = "“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.\”";
+    const text1 = "“After my knee surgery, the convenience of online consultations made my recovery smoother than I could have imagined.\"";
     const text2 = "\"Managing chronic conditions like diabetes requires a lot of vigilance, but the medicine refill system has simplified my life.\"";
     const text3 = "\"The prescription refill system is a game-changer for managing my diabetes. It’s really efficient and completely hassle-free.\"";
     const text4 = "\"Finding a doctor who really understands all of my health needs has never been easier. This platform has changed my life.\"";
@@ -26,8 +26,8 @@ const Testimonials = () => {
                     <img className="esfera-1" src={esfera} alt="" />
                     <img className="esfera-2" src={esfera} alt="" />
                     <div className='row-1 d-flex'> 
-                        <Testimony text={text1} autor="Linda A." imagen={test1}/>
-                        <Testimony text={text2} autor="Henry B." imagen={test2} />
+                        <Testimony text={text1} autor="- Linda A." imagen={test1}/>
+                        <Testimony text={text2} autor="- Henry B." imagen={test2} />
                     </div>
                     <div className='d-flex my-4 estadisticas'> 
                         <Estadistica stat="10,000+" informacion="Successful Consultations" />
@@ -38,8 +38,8 @@ const Testimonials = () => {
                         
                     </div>    
                     <div className='row-1 d-flex'>
-                        <Testimony text={text3} autor="Joshua T." imagen={test3} />
-                        <Testimony text={text4} autor="Samanta K." imagen={test4} />
+                        <Testimony text={text3} autor="- Joshua T." imagen={test3} />
+                        <Testimony text={text4} autor="- Samanta K." imagen={test4} />
                     </div>
                 </div>
             </div>
