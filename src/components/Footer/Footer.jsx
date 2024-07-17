2933c6f5f375082a739811309b0fad347f84c3b3
FacuMozo <facumozo98@gmail.com>
2024-07-16 21:10:04 -0300

    FooterSection Implementacion de footerSection diseño responsive se agregan todo los links, informacion, logos, e iconos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import './Footer.scss';
 
+import logo from '../../images/footer/logo.png'
+import facebookIcon from '../../images/footer/facebook.png'
+import instagramIcon from '../../images/footer/instagram.png'
+import linkedInIcon from '../../images/footer/linkedin.png'
+import youtubeIcon from '../../images/footer/youtube.png'
+
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
-                <p>&copy; 2024 Healthcare Inc. All rights reserved.</p>
-                <div className="social-icons">
-                    <a href="./"><i className="fab fa-facebook-f"></i></a>
-                    <a href="./"><i className="fab fa-twitter"></i></a>
-                    <a href="./"><i className="fab fa-instagram"></i></a>
+            <div className='start '>
+                <div className='columna-1  text-start'>
+                    <img src={logo} alt="" />
+            
+                    <p className='m-0'>Experience personalized medical <br /> care from the comfort of your home.</p>
                 </div>
+                <div className='links'>
+
+                    <div className='columna '>
+                        <span>Support</span>
+                        <a href="./">Getting Started</a>
+                        <a href="./">FAQS</a>
+                        <a href="./">Help Articles</a>
+                        <a href="./">Report an issue</a>
+                        <a href="./">Contact Help Desk</a>
+                    </div>
+                    <div className='columna '>
+                        <span>Services</span>
+                        <a href="./">Booking appointments</a>
+                        <a href="./">Online consultations</a>
+                        <a href="./">Prescriptions</a>
+                        <a href="./">Medicine Refills</a>
+                        <a href="./">Medical Notes</a>
+                    </div>
+                    <div className='columna '>
+                        <span>Legal</span>
+                        <a href="./">Terms & Conditions</a>
+                        <a href="./">Privacy Policy</a>
+                        <a href="./">Cookie Notice</a>
+                        <a href="./">Cookie Preferences</a>
+                        <a href="./">Trust Center</a>
+                    </div>
+                </div>
+            </div>
+            <hr className='' />
+            <div className="end d-flex justify-content-between">
+                <div className="social-icons">
+                    <a href="./"><img src={facebookIcon} alt="" /></a>
+                    <a href="./"><img src={instagramIcon} alt="" /></a>
+                    <a href="./"><img src={linkedInIcon} alt="" /></a>
+                    <a href="./"><img src={youtubeIcon} alt="" /></a>
+                </div>
+                <p className='m-0'> HealNet 2024 &copy; All Rights Reserved</p>
             </div>
         </footer>
     );
