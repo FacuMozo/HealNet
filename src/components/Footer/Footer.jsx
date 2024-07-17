5d17c3d21f9e60ec18e8a8ffc3f1156421749fb1
FacuMozo <facumozo98@gmail.com>
2024-07-16 23:33:16 -0300

    se corrigen margenes de separacion de secciones se corrige fontsize h2 en contactUs Se corrigen links en footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Footer.scss';
 
@@ -20,37 +21,37 @@ const Footer = () => {
 
                     <div className='columna '>
                         <span>Support</span>
-                        <a href="./">Getting Started</a>
-                        <a href="./">FAQS</a>
-                        <a href="./">Help Articles</a>
-                        <a href="./">Report an issue</a>
-                        <a href="./">Contact Help Desk</a>
+                        <a >Getting Started</a>
+                        <a >FAQS</a>
+                        <a >Help Articles</a>
+                        <a >Report an issue</a>
+                        <a >Contact Help Desk</a>
                     </div>
                     <div className='columna '>
                         <span>Services</span>
-                        <a href="./">Booking appointments</a>
-                        <a href="./">Online consultations</a>
-                        <a href="./">Prescriptions</a>
-                        <a href="./">Medicine Refills</a>
-                        <a href="./">Medical Notes</a>
+                        <a >Booking appointments</a>
+                        <a >Online consultations</a>
+                        <a >Prescriptions</a>
+                        <a >Medicine Refills</a>
+                        <a >Medical Notes</a>
                     </div>
                     <div className='columna '>
                         <span>Legal</span>
-                        <a href="./">Terms & Conditions</a>
-                        <a href="./">Privacy Policy</a>
-                        <a href="./">Cookie Notice</a>
-                        <a href="./">Cookie Preferences</a>
-                        <a href="./">Trust Center</a>
+                        <a  onclick="return false;">Terms & Conditions</a>
+                        <a  onclick="return false;">Privacy Policy</a>
+                        <a >Cookie Notice</a>
+                        <a >Cookie Preferences</a>
+                        <a >Trust Center</a>
                     </div>
                 </div>
             </div>
             <hr className='' />
             <div className="end d-flex justify-content-between">
                 <div className="social-icons">
-                    <a href="./"><img src={facebookIcon} alt="" /></a>
-                    <a href="./"><img src={instagramIcon} alt="" /></a>
-                    <a href="./"><img src={linkedInIcon} alt="" /></a>
-                    <a href="./"><img src={youtubeIcon} alt="" /></a>
+                    <a ><img src={facebookIcon} alt="" /></a>
+                    <a ><img src={instagramIcon} alt="" /></a>
+                    <a ><img src={linkedInIcon} alt="" /></a>
+                    <a ><img src={youtubeIcon} alt="" /></a>
                 </div>
                 <p className='m-0'> HealNet 2024 &copy; All Rights Reserved</p>
             </div>
